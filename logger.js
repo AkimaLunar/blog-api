e963d8e125f6f5f4352fe92ecb64c2e4ffc93664
Ria Carmin <akimalunar@gmail.com>
2017-05-23 20:59:57 -0700

    Annotations

--- logger.js
+++ logger.js
@@ -6,6 +6,7 @@ const chalk = require('chalk');
 const logger = new(winston.Logger)({
     transports: [
         new (winston.transports.Console)(),
+        // Doesn't work in now.sh
         // new (winston.transports.File)({ filename: 'logger.log' })
     ]
 });
