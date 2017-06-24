b39ec76f96d0650baec3b4410c413d7d296d5d9d
Ria Carmin <akimalunar@gmail.com>
2017-06-24 09:29:02 -0700

    0.0.1-2

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { PostListComponent } from './containers/post-list/post-list.component';
+
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { CardBlogComponent } from './components/card-blog/card-blog.component';
-import { PostListComponent } from './containers/post-list/post-list.component';
 
 @NgModule({
   declarations: [
