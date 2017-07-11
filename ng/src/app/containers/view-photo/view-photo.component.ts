a596b7a22dec8018f8be181868156a7eddd9867e
Ria Carmin <akimalunar@gmail.com>
2017-07-10 19:13:05 -0700

    User should be able to delete a post [ch60]
    - Photo view

--- ng/src/app/containers/view-photo/view-photo.component.ts
+++ ng/src/app/containers/view-photo/view-photo.component.ts
@@ -4,27 +4,41 @@ import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 
 import { Post } from '../../models/post';
+import { AuthService } from '../../services/auth.service';
 import { PostsService } from '../../services/posts.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-view-photo',
   templateUrl: './view-photo.component.html',
   styleUrls: ['./view-photo.component.css'],
-  providers: [PostsService]
+  providers: [AuthService, PostsService, UsersService]
 })
 export class ViewPhotoComponent implements OnInit {
   post: Post;
+  self: Boolean;
   contentChecked: Boolean;
   constructor(
     private postsService: PostsService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private auth: AuthService,
+    private usersService: UsersService
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap
       .switchMap((params: ParamMap) => this.postsService.getPostById(params.get('id')))
-      .subscribe(post => this.post = post);
+      .subscribe(post => {
+        this.post = post;
+        this.self = this.auth.self(this.post.author.userId);
+      });
   }
 
+  onEdit() {
+    alert('NOT READY!');
+  }
+  onDelete() {
+    this.postsService.deletePostById(this.post._id, this.post.author.userId);
+  }
 }
