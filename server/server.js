400d54f3767eff8de69b202acd8818e440da20aa
Ria Nicole Carmin <akimalunar@gmail.com>
2017-07-19 14:52:33 -0700

    Removed forced HTTPS

--- server/server.js
+++ server/server.js
@@ -11,19 +11,7 @@ const chalk           = require('chalk');
 const cors            = require('cors');
 const app             = express();
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
 app.use(cors());
-app.use(forceSSL());
 app.use(express.static(__dirname + '/public'));
 
 app.use('/api/posts', postsRouter);
